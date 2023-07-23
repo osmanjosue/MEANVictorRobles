fb347edb120a847a73cc8149f49df16cedd0d346
osmanjosue <osmanjosue@gmail.com>
2023-07-23 14:05:51 -0600

    70 seccion, 247 clase, Se creo el formulario para crear un proyecto

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from 'src/app/services/project.service';
+import { Project } from 'src/app/models/project';
+
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers: [ProjectService]
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public title:string;
+  public project:Project;
+  constructor( private _projectService:ProjectService) { 
+    this.title="crear Proyecto";
+    this.project=new Project('', '', '','', 2019, '', '');
+  }
 
   ngOnInit(): void {
+  }
+  
+  onSubmit(form:any){
+    console.log(this.project);
   }
 
 }
