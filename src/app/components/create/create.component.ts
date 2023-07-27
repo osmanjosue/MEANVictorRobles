f1f0c2af0dcf4b11a33b882f97753821ba762fd8
osmanjosue <osmanjosue@gmail.com>
2023-07-26 21:21:11 -0600

    70 seccion, 250 clase, 2 parte, se trabajo puros estilos y html

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -13,6 +13,8 @@ export class CreateComponent implements OnInit {
 
   public title:string;
   public project:Project;
+  public status:string;
+
   constructor( private _projectService:ProjectService) { 
     this.title="crear Proyecto";
     this.project=new Project('', '', '','', 2019, '', '');
@@ -22,10 +24,14 @@ export class CreateComponent implements OnInit {
   }
   
   onSubmit(form:any){
-    console.log(this.project);
     this._projectService.saveProject(this.project).subscribe(
       response=> {
-        console.log(response);
+        if(response.project){
+          this.status='success';
+          form.reset();
+        }else{
+          this.status='failed';
+        }
       },
       error=>{
         console.log(<any>error);
