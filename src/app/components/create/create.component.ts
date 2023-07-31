a6d3732e2f3c01b877ef04ea43aad9346b9c23c5
osmanjosue <osmanjosue@gmail.com>
2023-07-31 13:07:02 -0600

    70 seccion, 251 clase, Se crearon los servicios y funciones para subir imagenes al backend

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/services/project.service';
 import { Project } from 'src/app/models/project';
+import { UploadService } from 'src/app/services/upload.service';
+import { Global } from 'src/app/services/global';
 
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title:string;
   public project:Project;
   public status:string;
+  public filesToUpload:Array<File>;
 
-  constructor( private _projectService:ProjectService) { 
+  constructor( 
+    private _projectService:ProjectService,
+    private _uploadService:UploadService
+    ) { 
     this.title="crear Proyecto";
     this.project=new Project('', '', '','', 2019, '', '');
   }
@@ -24,11 +30,21 @@ export class CreateComponent implements OnInit {
   }
   
   onSubmit(form:any){
+
+    //Guardar datos basicos
     this._projectService.saveProject(this.project).subscribe(
       response=> {
         if(response.project){
-          this.status='success';
-          form.reset();
+         
+          
+          //subir la imagen
+          
+          this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image' )
+          .then((result:any)=>{
+            this.status='success';
+            console.log(result);
+            form.reset();
+          });
         }else{
           this.status='failed';
         }
@@ -37,6 +53,10 @@ export class CreateComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  fileChangeEvent(fileInput:any){
+    this.filesToUpload=<Array<File>>fileInput.target.files;
   }
 
 }
